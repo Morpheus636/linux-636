27e485e5ae7f6c86db76d2bc83bf61d93614b331
Josh Levin <floydaroo@gmail.com>
2021-10-27 23:48:23 -0400

    Changed how chrome is installed

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ apt update -y
 apt upgrade -y
 
 # Easy Installations
+apt install software-properties-common -y
+apt install apt-transport-https -y
+apt install wget -y
 apt install git -y
 apt install nodejs -y
 apt install gnome-tweaks -y
@@ -22,21 +25,23 @@ make local-install
 cd $scriptpath # CD back to the source dir.
 
 # Install Avidity Total Dusk Theme
-apt-get install gtk2-engines-murrine
+apt-get install gtk2-engines-murrine -y
 unzip ./Avidity-Total-Dusk.zip -d /usr/share/themes/
 # Install Avidity Total Dusk Icon Pack
 unzip ./Avidity-Total-Dusk-Suru-Reversed.zip -d /usr/share/icons/
 
 # Add Deadsnakes PPA (For multiple python versions)
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
+add-apt-repository ppa:deadsnakes/ppa
+apt update
 
 # Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+add-apt-repository "https://github.com/Morpheus636/desktop-config/blob/main/setup.sh"
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+apt update
+apt install google-chrome-stable -y
 
 # Install VSCode
-apt install software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-apt install code
+apt update
+apt install code -y
