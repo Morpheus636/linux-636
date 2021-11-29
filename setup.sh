46371b5450b4c37cc49992d8040c14370750c703
Josh Levin <floydaroo@gmail.com>
2021-11-29 12:42:17 -0500

    Not totally sure what it thinks I did

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 scriptpath="$(dirname $(readlink -f $0))/$(basename $0)"
 
 # Run install apps
+chmod +x ./install-apps.sh
 ./install-apps.sh
 
 # Run install theme
+chmod +x ./install-theme.sh
 ./install-theme.sh
-
