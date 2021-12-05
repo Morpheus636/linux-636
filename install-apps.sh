8ad6200ad07def3264294fe1963069ca0992873a
Josh Levin <floydaroo@gmail.com>
2021-12-04 20:26:16 -0500

    I messed this all up but now its fixed/.

--- install-apps.sh
+++ install-apps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 scriptpath="$(dirname $(readlink -f $0))/$(basename $0)"
+homedir=/home/$1
 
 # Update System
 apt-get update
@@ -13,19 +14,21 @@ apt-get install software-properties-common -y
 apt-get install apt-transport-https -y
 apt-get install wget -y
 
-# Easy Apps
+# Easy Apps - Apt
 apt-get install nodejs -y
 apt-get install gnome-tweaks -y
 apt-get install gnome-shell-extensions -y
-snap install pycharm-community --classic
-snap install discord
 apt-get install vim -y
 apt-get install zeal -y
 apt-get install openvpn -y
 apt-get install filezilla -y
 apt-get install build-essential -y
 
-# Pop Shell
+# Easy Apps - Snap
+snap install pycharm-community --classic
+snap install discord
+
+# Install Pop Shell
 apt-get install node-typescript -y make git
 git clone https://github.com/pop-os/shell
 cd ./shell # CD into the repo clone
@@ -59,25 +62,27 @@ apt-get install docker-ce docker-ce-cli conteinerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-
 # Install Eclipse
 # Dependency
-apt-get install default-jre -y
+apt-get install libcanberra-gtk-module -y
+apt-get install default-jdk -y
 
-# Install Eclipse Cpp to ~/eclipse/cpp/eclipse/eclipse
-wget https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/kepler/SR2/eclipse-cpp-kepler-SR2-linux-gtk-x86_64.tar.gz -O ./eclipse-cpp.tar.gz
-mkdir -p ~/eclipse/cpp/
-tar -xf ./eclipse-cpp.tar.gz -C ~/eclipse/cpp/
-# Symlink Eclipse Cpp to ~/bin/eclipse-cpp
-ln -s  ~/eclipse/cpp/eclipse/eclipse  ~/bin/eclipse-cpp
-# Delete the eclipsd cpp tarball
-rm -rf ./eclipse-cpp.tar.gz
+# Install Eclipse Cpp to $HOMEDIR/eclipse/cpp/eclipse/eclipse
+mkdir -p $HOMEDIR/eclipse/cpp/
+wget https://mirror.umd.edu/eclipse/technology/epp/downloads/release/2021-09/R/eclipse-cpp-2021-09-R-linux-gtk-x86_64.tar.gz -O - | tar -xz -C $HOMEDIR/eclipse/cpp/
+# Symlink Eclipse Cpp to $HOMEDIR/bin/eclipse-cpp
+ln -s  $HOMEDIR/eclipse/cpp/eclipse/eclipse  $HOMEDIR/.local/bin/eclipse-cpp
+# Add eclipse cpp to the applicaitons menu
+cp ./deps-assets/eclipse/eclipse-cpp.desktop $HOMEDIR/.local/share/applications/eclipse-cpp.desktop
+sed -i "/s/HOMEDIR/$HOMEDIR/g" $HOMEDIR/.local/share/applications/eclipse-cpp.desktop
+chmod +x $HOMEDIR/.local/share/applications/eclipse-cpp.desktop
 
-# Install Eclipse Java to ~/eclipse/java/eclipse/eclipse
-wget https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2021-09/R/eclipse-java-2021-09-R-linux-gtk-x86_64.tar.gz -O ./eclipse-java.tar.gz
-mkdir -p ~/eclipse/java/
-tar -xf ./eclipse-java.tar.gz -C ~/eclipse/java/
-# Symlink Eclipse Java to ~/bin/eclipse-java
-ln -s  ~/eclipse/java/eclipse/eclipse  ~/bin/eclipse-java
-# Delete the eclipsd Java tarball
-rm -rf ./eclipse-java.tar.gz
+# Install Eclipse Java to $HOMEDIR/eclipse/java/eclipse/eclipse
+mkdir -p $HOMEDIR/eclipse/java/
+wget https://mirror.umd.edu/eclipse/technology/epp/downloads/release/2021-09/R/eclipse-java-2021-09-R-linux-gtk-x86_64.tar.gz -O - | tar -xz -C $HOMEDIR/eclipse/java/
+# Symlink Eclipse Java to $HOMEDIR/bin/eclipse-java
+ln -s  $HOMEDIR/eclipse/java/eclipse/eclipse  $HOMEDIR/.local/bin/eclipse-java
+# Copy the .desktop file for eclipse java to add it to the applications menu
+cp ./deps-assets/eclipse/eclipse-java.desktop $HOMEDIR/.local/share/applications/eclipse-java.desktop
+sed -i "/s/HOMEDIR/$HOMEDIR/g" $HOMEDIR/.local/share/applications/eclipse-java.desktop
+chmod +x $HOMEDIR/.local/share/applications/eclipse-java.desktop
--- user-config.sh
+++ user-config.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 scriptpath="$(dirname $(readlink -f $0))/$(basename $0)"
+HOMEDIR=$1
 
 # Make repos directory
-mkdir ~/Documents/Repos
+mkdir $HOMEDIR/Documents/Repos
 
 # TODO - Configure OpenVPN
 # TODO - Git configuration
+# TODO - Gnome Tweaks Configuration
+# TODO - Set wallpaper
+# TODO - Configure Eclipse
