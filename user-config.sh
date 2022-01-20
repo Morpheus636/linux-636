3900fcc5711fc6a7de87444225361289e1deb849
Josh Levin <floydaroo@gmail.com>
2022-01-20 17:53:47 -0500

    updates to user_config

--- user-config.sh
+++ user-config.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+################################################################################
+# You can `source` this file to execute individual functions in it, but you need
+# to have your working directory set to the root of the repo, and you need to
+# pass $USER as an arg when you `source` it.
+
+# Functions must be run as sudo to work properly.
+
+# Ensure you run `sudo chown <username> ./home/<username>` after you run functions
+# To prevent files in your home directory from being owned by root.
+################################################################################
 #    © Copyright 2021 Josh Levin (Morpheus636 - https://github.com/morpheus636)
 # 
 #    This file is part of Morpheus636's desktop-config repository.
@@ -17,37 +27,70 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this repository.  If not, see <https://www.gnu.org/licenses/>.
 
-SCRIPTPATH="$(dirname $(dirname $(readlink -f $0))/$(basename $0))"
+WORKING_DIR=$PWD
 USERNAME=$1
 HOMEDIR=/home/$1
 
-# Make repos directory
-mkdir -p $HOMEDIR/Documents/Repos
+if [ $# -eq 0 ]
+  then
+    echo "You must pass \$USER as the only arg."
+    exit 1
+fi
 
-# BASH Configuration
-cp ./assets/config/.bashrc $HOMEDIR
-cp ./assets/config/.bash_aliases $HOMEDIR
+setup_fs(){
+  # Make repos directories
+  mkdir -p $HOMEDIR/src
+}
 
-# Set wallpaper
-mkdir -p $HOMEDIR/Pictures/wallpaper
-cp ./assets/wallpaper.jpg $HOMEDIR/Pictures/Wallpaper
-gsettings set org.gnome.desktop.background picture-uri "file:///${HOMEDIR}/Pictures/wallpaper/wallpaper.jpg"
+set_wallpaper(){
+  # Set wallpaper
+  mkdir -p $HOMEDIR/Pictures/wallpaper
+  cp ./assets/wallpaper.jpg $HOMEDIR/Pictures/Wallpaper
+  gsettings set org.gnome.desktop.background picture-uri "file:///${HOMEDIR}/Pictures/wallpaper/wallpaper.jpg"
+}
 
+set_pfp(){
+  # Set Profile Picture
+  cp ./assets/pfp.png /var/lib/AccountsService/icons/$USERNAME
+}
 
-# Set Profile Picture
-cp $SCRIPTPATH/assets/pfp.png /var/lib/AccountsService/icons/$USERNAME
+openvpn_config(){
+  # OpenVPN Config (if openvpn config file exists in assets/config)
+  if [[ -f "./config/client.ovpn" ]]; then
+    nmcli connection import type openvpn file ./assets/config/client.ovpn
+  fi
+}
+
+git_config(){
+  # Git configuration
+  cp ./assets/config/.gitconfig $HOMEDIR/
+}
+
+bash_config(){
+  # BASH Configuration
+  cp ./assets/config/.bashrc $HOMEDIR
+  cp ./assets/config/.bash_aliases $HOMEDIR
+}
 
 
 # TODO - Gnome Tweaks Configuration
 
-# Git configuration
-cp ./assets/config/.gitconfig $HOMEDIR/
-
-# OpenVPN Configuration
-if [[ -f "./config/client.ovpn" ]]; then
-    nmcli connection import type openvpn file ./assets/config/client.ovpn
-fi
-
 # TODO - Install Zeal Docsets
 
 # TODO - Thunderbird Configuration
+
+
+main(){
+  setup_fs
+  set_wallpaper
+  set_pfp
+  openvpn_config
+  git_config
+  bash_config
+
+  chown $USERNAME $HOMEDIR
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main "$@"
+fi
